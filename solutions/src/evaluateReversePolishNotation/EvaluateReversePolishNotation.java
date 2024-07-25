9ecc39adfc165d94e6de7b7f367baed831e3f9dd
Andrey <andrey1010102008@gmail.com>
2024-07-25 16:13:50 +0300

    add new solutions

---
+++ solutions/src/evaluateReversePolishNotation/EvaluateReversePolishNotation.java
@@ -0,0 +1,29 @@
+package evaluateReversePolishNotation;
+
+import java.util.ArrayDeque;
+import java.util.Deque;
+
+public class EvaluateReversePolishNotation {
+
+    public static int evalRPN(String[] tokens) {
+        Deque<Integer> stack = new ArrayDeque<>();
+
+        for (String token : tokens) {
+            if (!token.equals("+") && !token.equals("-") && !token.equals("*") && !token.equals("/")) {
+                stack.add(Integer.parseInt(token));
+            } else {
+                int secVal = stack.pollLast();
+                int firstVal = stack.pollLast();
+
+                switch (token) {
+                    case "+" -> stack.addLast(firstVal + secVal);
+                    case "-" -> stack.addLast(firstVal - secVal);
+                    case "*" -> stack.addLast(firstVal * secVal);
+                    default -> stack.addLast(firstVal / secVal);
+                }
+            }
+        }
+
+        return stack.poll();
+    }
+}
--- solutions/src/shortestPalindrome/ShortestPalindrome.java
+++ solutions/src/shortestPalindrome/ShortestPalindrome.java
@@ -1,7 +1,29 @@
 package shortestPalindrome;
 
 public class ShortestPalindrome {
-    public String shortestPalindrome(String s) {
-        return null;
+    public static String shortestPalindrome(String s) {
+        if (s.isEmpty()) {
+            return "";
+        }
+
+        char[] sChars = s.toCharArray();
+
+        int end = 0;
+        firstLoop: for (int i = s.length() - 1; i >= 0; i--) {
+            int l = 0;
+            int r = i;
+
+            while (r >= 0 && l < s.length() && sChars[l] == sChars[r]) {
+                if (l >= r) {
+                    end = i;
+                    break firstLoop;
+                }
+
+                l++;
+                r--;
+            }
+        }
+
+        return new StringBuilder(s).reverse() + s.substring(end + 1);
     }
 }
