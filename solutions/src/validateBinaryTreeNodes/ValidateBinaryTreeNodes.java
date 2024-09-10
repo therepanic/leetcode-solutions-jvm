c3c2c5908a9a278832d990ce75e46d7ddf93b57c
Andrey <andrey1010102008@gmail.com>
2024-09-10 16:41:19 +0300

    add new solutions

---
+++ solutions/src/validateBinaryTreeNodes/ValidateBinaryTreeNodes.java
@@ -0,0 +1,10 @@
+package validateBinaryTreeNodes;
+
+import java.util.*;
+
+public class ValidateBinaryTreeNodes {
+    public boolean validateBinaryTreeNodes(int n, int[] leftChild, int[] rightChild) {
+        //todo
+        return true;
+    }
+}
