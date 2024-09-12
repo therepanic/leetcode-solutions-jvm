1b53bab15eb8f5261cef11e096f7f10e1f56d4bc
Andrey <andrey1010102008@gmail.com>
2024-09-12 17:37:23 +0300

    add new solutions

--- solutions/src/combinations/Combinations.java
+++ solutions/src/combinations/Combinations.java
@@ -4,5 +4,27 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Combinations {
+    public List<List<Integer>> combine(int n, int k) {
+        List<List<Integer>> answer = new ArrayList<>();
 
+        helper(n, k, 1, answer, new ArrayList<>());
+
+        return answer;
+    }
+
+    public void helper(int n, int k, int curValue, List<List<Integer>> answer, List<Integer> perm) {
+        for (int i = curValue; i <= n; i++) {
+            if (perm.size() < k) {
+                perm.add(i);
+
+                helper(n, k, i + 1, answer, perm);
+
+                if (perm.size() == k) {
+                    answer.add(new ArrayList<>(perm));
+                }
+
+                perm.removeLast();
+            }
+        }
+    }
 }
