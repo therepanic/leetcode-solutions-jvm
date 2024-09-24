a9e8bb6693d1397570bdd5952f197d88b6e22df2
Andrey <andrey1010102008@gmail.com>
2024-09-24 18:11:34 +0300

    add new solutions

---
+++ solutions/src/cousinsInBinaryTreeTodo/CousinsInBinaryTree.java
@@ -0,0 +1,10 @@
+package cousinsInBinaryTreeTodo;
+
+import structures.treeNode.TreeNode;
+
+public class CousinsInBinaryTree {
+    public boolean isCousins(TreeNode root, int x, int y) {
+        //todo
+        return false;
+    }
+}
