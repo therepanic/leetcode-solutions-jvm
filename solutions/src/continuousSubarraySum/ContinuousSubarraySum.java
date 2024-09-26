e1eea1416645e19ae50fabab83b9390142ad0b92
Andrey <andrey1010102008@gmail.com>
2024-09-26 23:08:12 +0300

    add new solutions

--- solutions/src/continuousSubarraySum/ContinuousSubarraySum.java
+++ solutions/src/continuousSubarraySum/ContinuousSubarraySum.java
@@ -1,7 +1,25 @@
 package continuousSubarraySum;
 
-public class ContinuousSubarraySum {
-    public boolean checkSubarraySum(int[] nums, int k) {
+import java.util.HashMap;
+import java.util.Map;
 
+public class ContinuousSubarraySum {
+    public static boolean checkSubarraySum(int[] nums, int k) {
+        Map<Integer, Integer> map = new HashMap<>(Map.of(0, -1));
+
+        int prefix = 0;
+        for (int i = 0; i < nums.length; i++) {
+            prefix = (nums[i] + prefix) % k;
+
+            if (map.containsKey(prefix)) {
+                if (i - map.get(prefix) > 1) {
+                    return true;
+                }
+            } else {
+                map.put(prefix, i);
+            }
+        }
+
+        return false;
     }
 }
