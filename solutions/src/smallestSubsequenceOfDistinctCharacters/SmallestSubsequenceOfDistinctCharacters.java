e3297963de2e43a91dbcaf3ec1ab3bc58cd04204
Andrey <andrey1010102008@gmail.com>
2024-09-27 19:00:40 +0300

    add new solutions

---
+++ solutions/src/smallestSubsequenceOfDistinctCharacters/SmallestSubsequenceOfDistinctCharacters.java
@@ -0,0 +1,41 @@
+package smallestSubsequenceOfDistinctCharacters;
+
+import java.util.*;
+
+public class SmallestSubsequenceOfDistinctCharacters {
+    public String smallestSubsequence(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (char c : s.toCharArray()) {
+            if (map.containsKey(c)) {
+                map.put(c, map.get(c) + 1);
+            } else {
+                map.put(c, 1);
+            }
+        }
+
+        Set<Character> characterSet = new HashSet<>();
+        Deque<Character> stack = new ArrayDeque<>();
+
+        for (char c : s.toCharArray()) {
+            map.put(c, map.get(c) - 1);
+            if (characterSet.contains(c)) {
+                continue;
+            }
+
+            while (!stack.isEmpty() && stack.getLast() > c && map.get(stack.getLast()) > 0) {
+                characterSet.remove(stack.pollLast());
+            }
+
+            characterSet.add(c);
+            stack.addLast(c);
+        }
+
+        StringBuilder result = new StringBuilder();
+        for (char c : stack) {
+            result.append(c);
+        }
+
+        return result.toString();
+    }
+}
