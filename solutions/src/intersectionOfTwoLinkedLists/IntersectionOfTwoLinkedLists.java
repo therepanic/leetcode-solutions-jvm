e7bf9b867ef632fb3a6d228c6e984edd9bc31f9e
Andrey <andrey1010102008@gmail.com>
2024-09-29 13:02:18 +0300

    add new solutions

--- solutions/src/intersectionOfTwoLinkedLists/IntersectionOfTwoLinkedLists.java
+++ solutions/src/intersectionOfTwoLinkedLists/IntersectionOfTwoLinkedLists.java
@@ -2,31 +2,16 @@ package intersectionOfTwoLinkedLists;
 
 import structures.listNode.ListNode;
 
-import java.util.HashSet;
-import java.util.Set;
-
 public class IntersectionOfTwoLinkedLists {
     public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
-        Set<ListNode> listNodeSet = new HashSet<>();
+        ListNode a = headA;
+        ListNode b = headB;
 
-        while (headA != null || headB != null) {
-            if (headA != null) {
-                if (listNodeSet.contains(headA)) {
-                    return headA;
-                }
-                listNodeSet.add(headA);
-                headA = headA.next;
-            }
-
-            if (headB != null) {
-                if (listNodeSet.contains(headB)) {
-                    return headB;
-                }
-                listNodeSet.add(headB);
-                headB = headB.next;
-            }
+        while (a != b) {
+            a = a == null ? headB : a.next;
+            b = b == null ? headA : b.next;
         }
 
-        return null;
+        return b;
     }
 }
