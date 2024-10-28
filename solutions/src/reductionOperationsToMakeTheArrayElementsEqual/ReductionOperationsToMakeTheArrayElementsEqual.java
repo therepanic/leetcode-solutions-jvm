7be0687a2ccd367cb9b046a9f361150212670a31
Andrey <andrey1010102008@gmail.com>
2024-10-28 17:29:42 +0300

    add new solutions

--- solutions/src/reductionOperationsToMakeTheArrayElementsEqual/ReductionOperationsToMakeTheArrayElementsEqual.java
+++ solutions/src/reductionOperationsToMakeTheArrayElementsEqual/ReductionOperationsToMakeTheArrayElementsEqual.java
@@ -3,17 +3,14 @@ package reductionOperationsToMakeTheArrayElementsEqual;
 import java.util.Arrays;
 
 public class ReductionOperationsToMakeTheArrayElementsEqual {
-    public static void main(String[] args) {
-        System.out.println(reductionOperations(new int[] {7,9,10,8,6,4,1,5,2,3}));
-    }
     public static int reductionOperations(int[] nums) {
         Arrays.sort(nums);
 
         int operations = 0;
         int count = 0;
 
-        for (int i = 0; i < nums.length; i++) {
-            if (i > 0 && nums[i] > nums[i - 1]) {
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] > nums[i - 1]) {
                 count++;
             }
             operations += count;
