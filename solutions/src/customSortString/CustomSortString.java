883c038c227e637a48582078872eb4e500d9f911
Andrey <andrey1010102008@gmail.com>
2024-10-31 21:30:21 +0300

    add new solutions

---
+++ solutions/src/customSortString/CustomSortString.java
@@ -0,0 +1,35 @@
+package customSortString;
+
+import java.util.*;
+
+public class CustomSortString {
+    class Solution {
+        public String customSortString(String order, String s) {
+            Map<Character, Integer> map = new HashMap<>();
+
+            for(char c : s.toCharArray()) {
+                map.put(c, map.getOrDefault(c, 0) + 1);
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            for (char c : order.toCharArray()) {
+                if (map.containsKey(c)) {
+                    sb.append(String.valueOf(c).repeat(map.get(c)));
+                }
+
+                map.remove(c);
+            }
+
+            if (map.isEmpty()) {
+                return sb.toString();
+            }
+
+            for (char c : map.keySet()) {
+                sb.append(String.valueOf(c).repeat(map.get(c)));
+            }
+
+            return sb.toString();
+        }
+    }
+}
