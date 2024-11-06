7d665a79cf9e5672d0d88c4d81fd467bb9fbb505
Andrey Litvitski <andrey1010102008@gmail.com>
2024-11-06 22:45:31 +0300

    add new solutions

---
+++ solutions/src/applyOperationsToMakeStringEmpty/ApplyOperationsToMakeStringEmpty.java
@@ -0,0 +1,29 @@
+package applyOperationsToMakeStringEmpty;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ApplyOperationsToMakeStringEmpty {
+    public static String lastNonEmptyString(String s) {
+        char[] sChars = s.toCharArray();
+        int[] chars = new int[26];
+
+        int maxCount = 0;
+        for (char c : sChars) {
+            maxCount = Math.max(maxCount, ++chars[c - 'a']);
+        }
+        maxCount--;
+
+        int[] newChars = new int[26];
+        StringBuilder sb = new StringBuilder();
+
+        for (char c : sChars) {
+            if (++newChars[c - 'a'] > maxCount) {
+                sb.append(c);
+            }
+        }
+
+        return sb.toString();
+    }
+}
