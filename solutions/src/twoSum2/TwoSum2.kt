f7fe3ecb32e3dd05f010ae055911291057d6e4b4
Andrey Litvitski <andrey1010102008@gmail.com>
2024-11-14 18:10:17 +0300

    add new solutions

---
+++ solutions/src/twoSum2/TwoSum2.kt
@@ -0,0 +1,24 @@
+package twoSum2
+
+class TwoSum2 {
+    fun twoSum(numbers: IntArray, target: Int): IntArray {
+        numbers.sort()
+
+        var l = 0
+        var r = numbers.size - 1
+
+        while (l < r) {
+            val sum = numbers[l] + numbers[r]
+
+            if (sum == target) {
+                return intArrayOf(++l, ++r)
+            } else if (sum < target) {
+                l++
+            } else {
+                r--
+            }
+        }
+
+        return intArrayOf()
+    }
+}
