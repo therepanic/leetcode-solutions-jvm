1904447cba77d3e150efb787677e1f896a63a0e3
Andrey Litvitski <andrey1010102008@gmail.com>
2024-11-17 23:58:24 +0300

    add new solutions

--- solutions/src/addingSpacesToAString/AddingSpacesToAString.java
+++ solutions/src/addingSpacesToAString/AddingSpacesToAString.java
@@ -2,13 +2,28 @@ package addingSpacesToAString;
 
 public class AddingSpacesToAString {
     public String addSpaces(String s, int[] spaces) {
-        StringBuilder sb = new StringBuilder(s);
+        char[] sChars = s.toCharArray();
+        char[] chars = new char[s.length() + spaces.length];
+
+        int spaceIndex = 0;
+
         int prefix = 0;
-        for (int space : spaces) {
-            sb.insert(space + prefix, " ");
+
+        int i = 0;
+
+        while (i < sChars.length) {
+            if (sChars[i] + prefix == spaces[spaceIndex]) {
+                chars[i] = ' ';
+                spaceIndex++;
+                prefix++;
+                continue;
+            }
+
+            chars[i] = sChars[i];
             prefix++;
+            i++;
         }
 
-        return sb.toString();
+        return String.valueOf(chars);
     }
 }
