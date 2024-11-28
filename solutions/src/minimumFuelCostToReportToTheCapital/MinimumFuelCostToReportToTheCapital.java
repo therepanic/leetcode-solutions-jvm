2a279357767a67b79cfb8ed0510bad1421160243
Andrey Litvitski <andrey1010102008@gmail.com>
2024-11-28 18:31:11 +0300

    add new solutions

--- solutions/src/minimumFuelCostToReportToTheCapital/MinimumFuelCostToReportToTheCapital.java
+++ solutions/src/minimumFuelCostToReportToTheCapital/MinimumFuelCostToReportToTheCapital.java
@@ -5,25 +5,7 @@ import java.util.List;
 
 public class MinimumFuelCostToReportToTheCapital {
     public long minimumFuelCost(int[][] roads, int seats) {
-        long sum = 0;
-        List<List<Integer>> graph = new ArrayList<>();
-        int n = Integer.MIN_VALUE;
-
-        for (int[] road : roads) {
-            n = Math.max(n, road[0]);
-            n = Math.max(n, road[1]);
-        }
-
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-
-        for (int i = 0; i < roads.length; i++) {
-            graph.get(roads[i][1]).add(roads[i][10]);
-        }
-
-
+    //TODO:
+        return 0;
     }
-
-    public void dfs()
 }
