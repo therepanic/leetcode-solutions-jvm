77bd3d78a05f4891ad6748d8ae4a912eebb6ba72
Andrey Litvitski <andrey1010102008@gmail.com>
2024-12-12 22:06:35 +0300

    add new solutions

---
+++ solutions/src/nearestExitFromEntranceInMaze/NearestExitFromEntranceInMaze.java
@@ -0,0 +1,56 @@
+package nearestExitFromEntranceInMaze;
+
+import java.util.*;
+
+public class NearestExitFromEntranceInMaze {
+    public int nearestExit(char[][] maze, int[] entrance) {
+        Queue<int[]> move = new ArrayDeque<>();
+
+        for (int i = 0; i < maze.length; i++) {
+            if (maze[i][0] == '.' && (i != entrance[0] || 0 != entrance[1])) {
+                move.add(new int[] {i, 0, 0});
+            }
+
+            if (maze[i][maze[i].length - 1] == '.' && (i != entrance[0] || maze[i].length - 1 != entrance[1])) {
+                move.add(new int[] {i, maze[i].length - 1, 0});
+            }
+        }
+
+        for (int i = 0; i < maze[0].length; i++) {
+            if (maze[0][i] == '.' && (0 != entrance[0] || i != entrance[1])) {
+                move.add(new int[] {0, i, 0});
+            }
+
+            if (maze[maze.length - 1][i] == '.' && (maze.length - 1 != entrance[0] || i != entrance[1])) {
+                move.add(new int[] {maze.length - 1, i, 0});
+            }
+        }
+
+        Set<String> set = new HashSet<>();
+
+        while (!move.isEmpty()) {
+            int[] poll = move.poll();
+
+            String key = poll[0] + "-" + poll[1];
+
+            if (set.contains(key)) continue;
+
+            if (poll[0] < 0 || poll[1] < 0 || poll[0] >= maze.length || poll[1] >= maze[0].length || maze[poll[0]][poll[1]] == '+') {
+                continue;
+            }
+
+            if (poll[0] == entrance[0] && poll[1] == entrance[1]) {
+                return poll[2];
+            }
+
+            set.add(key);
+
+            move.add(new int[] {poll[0] + 1, poll[1], poll[2] + 1});
+            move.add(new int[] {poll[0] - 1, poll[1], poll[2] + 1});
+            move.add(new int[] {poll[0], poll[1] + 1, poll[2] + 1});
+            move.add(new int[] {poll[0], poll[1] - 1, poll[2] + 1});
+        }
+
+        return -1;
+    }
+}
