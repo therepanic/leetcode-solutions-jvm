c8fbff6ca6e0b919fd9fb9898ecc6be8de1cd84d
Andrey Litvitski <andrey1010102008@gmail.com>
2025-01-03 19:59:29 +0300

    new solutions

---
+++ solutions/src/minimumAverageDifference/MinimumAverageDifference.java
@@ -0,0 +1,44 @@
+package minimumAverageDifference;
+
+import java.util.Arrays;
+
+public class MinimumAverageDifference {
+    public static int minimumAverageDifference(int[] nums) {
+        long[] left = new long[nums.length];
+        long[] right = new long[nums.length];
+
+        long sum = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+
+            left[i] = sum / (i + 1);
+        }
+
+        sum = 0;
+
+        for (int i = right.length - 1; i >= 0; i--) {
+            sum += nums[i];
+
+            right[i] = (long) (double) sum / (right.length - i);
+        }
+
+
+        int min = Integer.MAX_VALUE;
+        long val = Integer.MAX_VALUE;;
+        for (int i = 0; i < nums.length; i++) {
+            long sumd = left[i];
+
+            if (i != nums.length - 1) {
+                sumd = Math.abs(sumd - right[i + 1]);
+            }
+
+            if (sumd < val) {
+                val = sumd;
+                min = i;
+            }
+        }
+
+        return min;
+    }
+}
