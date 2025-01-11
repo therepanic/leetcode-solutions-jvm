1198a23d646be37e2d335beef032f0562dac7a69
Andrey Litvitski <andrey1010102008@gmail.com>
2025-01-11 14:49:19 +0300

    new solutions

--- solutions/src/constructKPalindromeStrings/ConstructKPalindromeStrings.java
+++ solutions/src/constructKPalindromeStrings/ConstructKPalindromeStrings.java
@@ -1,27 +1,20 @@
 package constructKPalindromeStrings;
 
-import java.util.HashMap;
-import java.util.Map;
-
 public class ConstructKPalindromeStrings {
     public boolean canConstruct(String s, int k) {
         if (s.length() < k) return false;
 
-        Map<Character, Integer> charCountMap = new HashMap<>();
+        int[] charCount = new int[26];
 
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
 
-            if (charCountMap.containsKey(c)) {
-                charCountMap.put(c, charCountMap.get(c) + 1);
-            } else {
-                charCountMap.put(c, 1);
-            }
+            charCount[c - 'a']++;
         }
 
         int c = 0;
-        for (int value : charCountMap.values()) {
-            if (value % 2 != 0) {
+        for (int i = 0; i < 26; i++) {
+            if (charCount[i] % 2 != 0) {
                 c++;
             }
         }
