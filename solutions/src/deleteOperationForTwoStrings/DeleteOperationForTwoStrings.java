3ca650168fe5a947a903c974e698fa2c1e4099b1
Andrey Litvitski <andrey1010102008@gmail.com>
2025-01-20 21:03:36 +0300

    new solutions

--- solutions/src/deleteOperationForTwoStrings/DeleteOperationForTwoStrings.java
+++ solutions/src/deleteOperationForTwoStrings/DeleteOperationForTwoStrings.java
@@ -1,27 +1,25 @@
-package editDistance;
+package deleteOperationForTwoStrings;
 
-
-public class EditDistance {
+public class DeleteOperationForTwoStrings {
     public static int minDistance(String word1, String word2) {
-        int[][] dp = new int[word1.length() + 1][word2.length() + 1];
+        int[][] dp = new int[word2.length() + 1][word1.length() + 1];
 
-        for (int i = 0; i < dp[0].length; i++) {
-            dp[0][i] = i;
-        }
         for (int i = 0; i < dp.length; i++) {
             dp[i][0] = i;
         }
+        for (int i = 0; i < dp[0].length; i++) {
+            dp[0][i] = i;
+        }
         char[] word1Chars = word1.toCharArray();
         char[] word2Chars = word2.toCharArray();
-
         for (int i = 1; i < dp.length; i++) {
             for (int j = 1; j < dp[i].length; j++) {
-                if (word1Chars[i - 1] == word2Chars[j - 1]) {
+                if (word2Chars[i - 1] == word1Chars[j - 1]) {
                     dp[i][j] = dp[i - 1][j - 1];
                     continue;
                 }
-                int c = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
-                dp[i][j] = c;
+
+                dp[i][j] = Math.min(dp[i][j - 1] + 1, dp[i - 1][j] + 1);
             }
         }
         return dp[dp.length - 1][dp[0].length - 1];
