a21fe96cd01d086be646b1de79e0036e12241250
Andrey Litvitski <andrey1010102008@gmail.com>
2025-01-23 17:50:01 +0300

    new solutions

---
+++ solutions/src/jumpGameIII/JumpGameIII.java
@@ -0,0 +1,9 @@
+package jumpGameIII;
+
+public class JumpGameIII {
+    public boolean canReach(int[] arr, int start) {
+        if (start < 0 || start >= arr.length || arr[start] < 0) return false;
+        arr[start] *= -1;
+        return arr[start] == 0 || canReach(arr, start - arr[start]) || canReach(arr, start + arr[start]);
+    }
+}
