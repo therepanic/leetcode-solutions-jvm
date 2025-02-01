f94bddd33f0889936014ec5a2bf3c74b2958529c
Andrey Litvitski <andrey1010102008@gmail.com>
2025-02-01 13:05:16 +0300

    new solutions

---
+++ solutions/src/specialArrayI/SpecialArrayI.java
@@ -0,0 +1,12 @@
+package specialArrayI;
+
+public class SpecialArrayI {
+    public boolean isArraySpecial(int[] nums) {
+        for (int i = 1 ; i < nums.length; i++) {
+            if (nums[i] % 2 == 0 && nums[i - 1] % 2 == 0 || nums[i] % 2 != 0 && nums[i - 1] % 2 != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
