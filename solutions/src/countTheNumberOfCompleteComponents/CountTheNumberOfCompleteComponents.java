983abf4ebdf3771456b7d0c9becebf5d588099ab
Andrey Litvitski <andrey1010102008@gmail.com>
2025-02-04 17:58:10 +0300

    new solutions

--- solutions/src/countTheNumberOfCompleteComponents/CountTheNumberOfCompleteComponents.java
+++ solutions/src/countTheNumberOfCompleteComponents/CountTheNumberOfCompleteComponents.java
@@ -3,60 +3,41 @@ package countTheNumberOfCompleteComponents;
 import java.util.*;
 
 public class CountTheNumberOfCompleteComponents {
-    public int[] minimumTime(int n, int[][] edges, int[] disappear) {
-        List<List<int[]>> graph = new ArrayList<>();
-
+    public int countCompleteComponents(int n, int[][] edges) {
+        boolean[] checked = new boolean[n];
+        List<List<Integer>> graph = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
-
-        for (int i = 0; i < edges.length; i++) {
-            graph.get(edges[i][0]).add(new int[] {edges[i][1], edges[i][2]});
-            graph.get(edges[i][1]).add(new int[] {edges[i][0], edges[i][2]});
+        for (int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
         }
-
-        int[] minimumTime = new int[n];
-        Arrays.fill(minimumTime, Integer.MAX_VALUE);
-        minimumTime[0] = 0;
-
-        PriorityQueue<int[]> priorityQueue = new PriorityQueue<>(Comparator.comparingInt(a -> a[1]));
-
-        priorityQueue.add(new int[] {0, 0});
-
-        boolean[] visited = new boolean[n];
-
-        while (!priorityQueue.isEmpty()) {
-            int[] edge = priorityQueue.poll();
-
-            int nextNode = edge[0];
-            int currentTime = edge[1];
-
-            if (visited[nextNode]) continue;
-
-            visited[nextNode] = true;
-
-            if (currentTime >= disappear[nextNode]) {
-                continue;
-            }
-
-            for (int[] node : graph.get(nextNode)) {
-                int newTime = node[1] + currentTime;
-
-                if (newTime < disappear[node[0]] && newTime < minimumTime[node[0]]) {
-                    minimumTime[node[0]] = newTime;
-
-                    priorityQueue.add(new int[] {node[0], newTime});
+        int count = 0;
+        for (int i = 0; i < n; i++) {
+            if (checked[i]) continue;
+            List<Integer> nodes = new ArrayList<>();
+            int c = count(i, graph, checked, nodes);
+            boolean ok = true;
+            for (int node : nodes) {
+                if (graph.get(node).size() != c - 1) {
+                    ok = false;
+                    break;
                 }
             }
+            if (ok) count++;
         }
+        return count;
+    }
 
-
-        for (int i = 0; i < minimumTime.length; i++) {
-            if (minimumTime[i] > disappear[i]) {
-                minimumTime[i] = -1;
-            }
+    public int count(int node, List<List<Integer>> graph, boolean[] checked, List<Integer> nodes) {
+        if (checked[node]) return 0;
+        checked[node] = true;
+        nodes.add(node);
+        int count = 1;
+        for (int curNode : graph.get(node)) {
+            count += count(curNode, graph, checked, nodes);
         }
-
-        return minimumTime;
+        return count;
     }
 }
