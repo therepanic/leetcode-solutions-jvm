80f107ac34c873a769bf72c9f1b6665d34801c02
Andrey Litvitski <andrey1010102008@gmail.com>
2025-02-11 17:32:31 +0300

    new solutions

---
+++ solutions/src/removeAllOccurrencesOfASubstring/RemoveAllOccurrencesOfASubstring.java
@@ -0,0 +1,12 @@
+package removeAllOccurrencesOfASubstring;
+
+public class RemoveAllOccurrencesOfASubstring {
+    public static String removeOccurrences(String s, String part) {
+        StringBuilder sb = new StringBuilder(s);
+        while (sb.indexOf(part) != -1) {
+            int index = sb.indexOf(part);
+            sb.delete(index, index + part.length());
+        }
+        return sb.toString();
+    }
+}
