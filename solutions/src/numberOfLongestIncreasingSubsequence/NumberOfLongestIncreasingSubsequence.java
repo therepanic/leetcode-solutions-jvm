0a19655b7f1a116918fc29c2d02c80330ab6c6b6
Andrey Litvitski <andrey1010102008@gmail.com>
2025-02-27 23:59:50 +0300

    new solutions

---
+++ solutions/src/numberOfLongestIncreasingSubsequence/NumberOfLongestIncreasingSubsequence.java
@@ -0,0 +1,41 @@
+package numberOfLongestIncreasingSubsequence;
+
+import java.util.Arrays;
+
+public class NumberOfLongestIncreasingSubsequence {
+    public static void main(String[] args) {
+        System.out.println(findNumberOfLIS(new int[] {2,2,2,2,1}));
+        System.out.println(findNumberOfLIS(new int[] {2,1}));
+        System.out.println(findNumberOfLIS(new int[] {1,3,5,4,7}));
+        System.out.println(findNumberOfLIS(new int[] {4,6,7,7}));
+    }
+    public static int findNumberOfLIS(int[] nums) {
+        int[] dp = new int[nums.length];
+        int[] count = new int[nums.length];
+        Arrays.fill(dp, 1);
+        Arrays.fill(count, 1);
+        for (int i = 1; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] < nums[i]) {
+                    if (dp[j] + 1 > dp[i]) {
+                        dp[i] = dp[j] + 1;
+                        count[i] = count[j];
+                    } else if (dp[j] + 1 == dp[i]) {
+                        count[i] += count[j];
+                    }
+                }
+            }
+        }
+        int max = 0;
+        for (int len : dp) {
+            max = Math.max(max, len);
+        }
+        int answer = 0;
+        for (int i = 0; i < dp.length; i++) {
+            if (dp[i] == max) {
+                answer += count[i];;
+            }
+        }
+        return answer;
+    }
+}
