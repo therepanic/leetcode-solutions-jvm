946832e2a4452f9fbe67957b53ac94331d5f83bb
Andrey Litvitski <andrey1010102008@gmail.com>
2025-03-25 16:11:23 +0300

    new solutions

---
+++ solutions/src/optimalPartitionOfString/OptimalPartitionOfString.java
@@ -0,0 +1,20 @@
+package optimalPartitionOfString;
+
+import java.util.Arrays;
+
+public class OptimalPartitionOfString {
+    public int partitionString(String s) {
+        char[] sChars = s.toCharArray();
+        int c = 1;
+        boolean[] has = new boolean[26];
+        for (char ch : sChars) {
+            int index = ch - 'a';
+            if (has[index]) {
+                Arrays.fill(has, false);
+                c++;;
+            }
+            has[index] = true;
+        }
+        return c;
+    }
+}
