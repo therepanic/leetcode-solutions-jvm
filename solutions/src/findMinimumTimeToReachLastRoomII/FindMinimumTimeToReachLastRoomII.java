12c587c07831611214f617feb1e50b9bef0ec60b
Andrey Litvitski <andrey1010102008@gmail.com>
2025-04-26 14:56:39 +0300

    new solutions

---
+++ solutions/src/findMinimumTimeToReachLastRoomII/FindMinimumTimeToReachLastRoomII.java
@@ -0,0 +1,40 @@
+package findMinimumTimeToReachLastRoomII;
+
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+
+public class FindMinimumTimeToReachLastRoomII {
+    public static int minTimeToReach(int[][] moveTime) {
+        PriorityQueue<int[]> heap = new PriorityQueue<>(Comparator.comparingInt(p -> p[2]));
+        heap.add(new int[] {0, 0, 0, 0});
+        int[][] times = new int[moveTime.length][moveTime[0].length];
+        for (int[] time : times) {
+            Arrays.fill(time, Integer.MAX_VALUE);
+        }
+        while (!heap.isEmpty()) {
+            int[] poll = heap.poll();
+            if (times[poll[0]][poll[1]] <= poll[2]) {
+                continue;
+            }
+            times[poll[0]][poll[1]] = Math.min(times[poll[0]][poll[1]], poll[2]);
+            if (poll[0] == moveTime.length - 1 && poll[1] == moveTime[0].length - 1) {
+                return poll[2];
+            }
+            int pref = poll[3] == 0 ? 1 : 2;
+            if (poll[0] + 1 < moveTime.length) {
+                heap.add(new int[] {poll[0] + 1, poll[1], (poll[2] < moveTime[poll[0] + 1][poll[1]] ? moveTime[poll[0] + 1][poll[1]] + pref : poll[2] + pref), poll[3] == 1 ? 0 : 1});
+            }
+            if (poll[1] + 1 < moveTime[0].length) {
+                heap.add(new int[] {poll[0], poll[1] + 1, (poll[2] < moveTime[poll[0]][poll[1] + 1] ? moveTime[poll[0]][poll[1] + 1] + pref : poll[2] + pref), poll[3] == 1 ? 0 : 1});
+            }
+            if (poll[0] - 1 >= 0) {
+                heap.add(new int[] {poll[0] - 1, poll[1], (poll[2] < moveTime[poll[0]- 1][poll[1]] ? moveTime[poll[0] - 1][poll[1]] + pref : poll[2] + pref), poll[3] == 1 ? 0 : 1});
+            }
+            if (poll[1] - 1 >= 0) {
+                heap.add(new int[] {poll[0], poll[1] - 1, (poll[2] < moveTime[poll[0]][poll[1] - 1] ? moveTime[poll[0]][poll[1] - 1] + pref : poll[2] + pref), poll[3] == 1 ? 0 : 1});
+            }
+        }
+        return -1;
+    }
+}
