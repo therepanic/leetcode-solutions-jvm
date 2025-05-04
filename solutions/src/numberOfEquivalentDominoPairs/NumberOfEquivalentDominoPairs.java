3d37a2219dbe297fcd50ec98953b3d8c5df2d4c0
Andrey Litvitski <andrey1010102008@gmail.com>
2025-05-04 13:18:35 +0300

    new solutions

---
+++ solutions/src/numberOfEquivalentDominoPairs/NumberOfEquivalentDominoPairs.java
@@ -0,0 +1,17 @@
+package numberOfEquivalentDominoPairs;
+
+import java.util.*;
+
+public class NumberOfEquivalentDominoPairs {
+    public int numEquivDominoPairs(int[][] dominoes) {
+        Map<List<Integer>, Integer> countMap = new HashMap<>();
+        int count = 0;
+        for (int[] domino : dominoes) {
+            Arrays.sort(domino);
+            List<Integer> key = new ArrayList<>(List.of(domino[0], domino[1]));
+            count += countMap.getOrDefault(key, 0);
+            countMap.put(key, countMap.getOrDefault(key, 0) + 1);
+        }
+        return count;
+    }
+}
