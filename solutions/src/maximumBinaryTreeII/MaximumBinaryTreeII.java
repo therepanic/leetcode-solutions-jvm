16546e535db5791dbd3eeb86653c8a3ca8435d3e
Andrey Litvitski <andrey1010102008@gmail.com>
2025-05-04 13:45:05 +0300

    new solutions

---
+++ solutions/src/maximumBinaryTreeII/MaximumBinaryTreeII.java
@@ -0,0 +1,16 @@
+package maximumBinaryTreeII;
+
+import structures.treeNode.TreeNode;
+
+public class MaximumBinaryTreeII {
+    public TreeNode insertIntoMaxTree(TreeNode root, int val) {
+        if (root == null) return new TreeNode(val);
+        if (val > root.val) {
+            TreeNode newRoot = new TreeNode(val);
+            newRoot.left = root;
+            return newRoot;
+        }
+        root.right = insertIntoMaxTree(root.right, val);
+        return root;
+    }
+}
