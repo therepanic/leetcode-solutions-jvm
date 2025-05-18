203f6e1516e5ac98cce270664b177e8cd502e765
Andrey Litvitski <andrey1010102008@gmail.com>
2025-05-18 23:58:59 +0300

    new solutions

---
+++ solutions/src/maximumCoinsFromKConsecutiveBags/MaximumCoinsFromKConsecutiveBags.java
@@ -0,0 +1,22 @@
+package maximumCoinsFromKConsecutiveBags;
+
+import java.util.Arrays;
+
+public class MaximumCoinsFromKConsecutiveBags {
+    public static void main(String[] args) {
+        System.out.println(maximumCoins(new int[][] {{8, 10, 1}, {1, 3, 2}, {5, 6, 4}}, 4));
+    }
+    public static long maximumCoins(int[][] coins, int k) {
+        Arrays.sort(coins, (p1, p2) -> {
+            if (p1[0] == p2[0]) {
+                return Integer.compare(p1[1], p2[1]);
+            } else {
+                return Integer.compare(p1[0], p2[0]);
+            }
+        });
+        long answer = 0;
+        long c = 0;
+        //TODO
+        return answer;
+    }
+}
