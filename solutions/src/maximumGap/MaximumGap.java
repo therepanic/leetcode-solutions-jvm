20547337fbc729a52e553eec2dcf4d64f92dd078
Andrey Litvitski <andrey1010102008@gmail.com>
2025-05-19 23:55:46 +0300

    new solutions

---
+++ solutions/src/maximumGap/MaximumGap.java
@@ -0,0 +1,14 @@
+package maximumGap;
+
+import java.util.Arrays;
+
+public class MaximumGap {
+    public int maximumGap(int[] nums) {
+        Arrays.sort(nums);
+        int max = 0;
+        for (int i = 1; i < nums.length; i++) {
+            max = Math.max(max, nums[i] - nums[i - 1]);
+        }
+        return max;
+    }
+}
