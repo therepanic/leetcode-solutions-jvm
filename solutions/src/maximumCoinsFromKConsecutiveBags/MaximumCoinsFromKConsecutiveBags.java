33a9a665cc155d4728acc0b0e54bab2d10aae1ee
Andrey Litvitski <andrey1010102008@gmail.com>
2025-05-19 19:22:35 +0300

    new solutions

--- solutions/src/maximumCoinsFromKConsecutiveBags/MaximumCoinsFromKConsecutiveBags.java
+++ solutions/src/maximumCoinsFromKConsecutiveBags/MaximumCoinsFromKConsecutiveBags.java
@@ -20,7 +20,28 @@ public class MaximumCoinsFromKConsecutiveBags {
         for (int i = 0; i < coins.length; i++) {
             int start = coins[i][0];
             int end = start + k - 1;
-            //TODO
+            while (it < coins.length && end >= coins[it][1]) {
+                c += ((coins[it][1] - (long) coins[it][0] + 1) * coins[it][2]);
+                it++;
+            }
+            if (it < coins.length) {
+                answer = Math.max(answer, c + Math.max(0, ((long) end - coins[it][0] + 1)) * coins[it][2]);
+            }
+            c -= (coins[i][1] - coins[i][0] + 1) * (long) coins[i][2];
+        }
+        c = 0;
+        it = 0;
+        for (int i = 0; i < coins.length; i++) {
+            int end = coins[i][1];
+            int start = end - k + 1;
+            c += ((long) (coins[i][1] - coins[i][0] + 1) * coins[i][2]);
+            while (it < coins.length && start > coins[it][1]) {
+                c -= ((coins[it][1] - (long) coins[it][0] + 1) * coins[it][2]);
+                it++;
+            }
+            if (it < coins.length) {
+                answer = Math.max(answer, c - Math.max(0, ((long) start - coins[it][0])) * coins[it][2]);
+            }
         }
         return answer;
     }
