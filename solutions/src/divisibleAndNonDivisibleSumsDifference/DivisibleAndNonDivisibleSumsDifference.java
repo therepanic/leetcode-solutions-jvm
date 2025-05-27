fd80f2532760c52e1646bf4be9d7233a19a656ce
Andrey Litvitski <andrey1010102008@gmail.com>
2025-05-27 13:30:45 +0300

    new solutions

---
+++ solutions/src/divisibleAndNonDivisibleSumsDifference/DivisibleAndNonDivisibleSumsDifference.java
@@ -0,0 +1,15 @@
+package divisibleAndNonDivisibleSumsDifference;
+
+public class DivisibleAndNonDivisibleSumsDifference {
+    public int differenceOfSums(int n, int m) {
+        int num1 = 0, num2 = 0;
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) {
+                num2 += i;
+            } else {
+                num1 += i;
+            }
+        }
+        return num1 - num2;
+    }
+}
