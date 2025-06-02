0c4474c5fe3873ee16b5aa73fea283ffe4159b9c
Andrey Litvitski <andrey1010102008@gmail.com>
2025-06-02 19:37:14 +0300

    new solutions

---
+++ solutions/src/findTheLongestSemiRepetitiveSubstring/FindTheLongestSemiRepetitiveSubstring.java
@@ -0,0 +1,28 @@
+package findTheLongestSemiRepetitiveSubstring;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class FindTheLongestSemiRepetitiveSubstring {
+
+    public static int longestSemiRepetitiveSubstring(String s) {
+        char[] sChars = s.toCharArray();
+        int l = 0;
+        int answer = 1;
+        int repetitive = 0;
+        for (int r = 1; r < sChars.length; r++) {
+            if (sChars[r] == sChars[r - 1]) {
+                repetitive++;
+            }
+            while (repetitive > 1) {
+                l++;
+                if (sChars[l] == sChars[l - 1]) {
+                    repetitive--;
+                }
+            }
+            answer = Math.max(answer, r - l + 1);
+        }
+        return answer;
+    }
+
+}
