87502e897abe8889b5df76766bb50260751611a6
Andrey Litvitski <andrey1010102008@gmail.com>
2025-06-25 15:45:03 +0300

    new solutions

--- solutions/src/maximumSwap/MaximumSwap.java
+++ solutions/src/maximumSwap/MaximumSwap.java
@@ -1,27 +1,31 @@
 package maximumSwap;
 
 public class MaximumSwap {
+
     public int maximumSwap(int num) {
-        StringBuilder numSb = new StringBuilder(String.valueOf(num));
-
-        int max = num;
-
-        for (int i = 0; i < numSb.length(); i++) {
-            for (int j = i + 1; j < numSb.length(); j++) {
-                char c = numSb.charAt(i);
-
-                numSb.setCharAt(i, numSb.charAt(j));
-                numSb.setCharAt(j, c);
-
-                max = Math.max(max, Integer.parseInt(numSb.toString()));
-
-                c = numSb.charAt(i);
-
-                numSb.setCharAt(i, numSb.charAt(j));
-                numSb.setCharAt(j, c);
+        char[] numChars = String.valueOf(num).toCharArray();
+        int n = numChars.length;
+        int[] maxArr = new int[n];
+        int maxVal = numChars[n - 1] - '0';
+        int[] indexMaxVal = new int[n];
+        for (int i = n - 1; i >= 0; i--) {
+            if (numChars[i] - '0' > maxVal) {
+                indexMaxVal[i] = i;
+                maxVal = numChars[i] - '0';
+            } else {
+                indexMaxVal[i] = i == n - 1 ? i : indexMaxVal[i + 1];
+            }
+            maxArr[i] = maxVal;
+        }
+        for (int i = 0; i < n - 1; i++) {
+            if (numChars[i] - '0' < maxArr[i]) {
+                char maxArrVal = (char) (maxArr[i] + '0');
+                numChars[indexMaxVal[i]] = numChars[i];
+                numChars[i] = maxArrVal;
+                return Integer.parseInt(String.valueOf(numChars));
             }
         }
-
-        return max;
+        return Integer.parseInt(String.valueOf(numChars));
     }
+
 }
