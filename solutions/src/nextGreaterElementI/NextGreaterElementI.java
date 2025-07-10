68accb3929459aaf238b8b09d92e1c2691b9415c
Andrey Litvitski <andrey1010102008@gmail.com>
2025-07-10 16:01:47 +0300

    new solutions

---
+++ solutions/src/nextGreaterElementI/NextGreaterElementI.java
@@ -0,0 +1,31 @@
+package nextGreaterElementI;
+
+import java.util.*;
+
+public class NextGreaterElementI {
+
+    public int[] nextGreaterElement(int[] nums1, int[] nums2) {
+        Map<Integer, Integer> containMap = new HashMap<>();
+        int[] pre = new int[nums2.length];
+        Arrays.fill(pre, -1);
+        Deque<int[]> deq = new ArrayDeque<>();
+        for (int i = 0; i < nums2.length; i++) {
+            while (!deq.isEmpty() && deq.getLast()[1] < nums2[i]) {
+                int[] poll = deq.pollLast();
+                pre[poll[0]] = nums2[i];
+            }
+            deq.addLast(new int[] {i, nums2[i]});
+        }
+        for (int i = 0; i < nums2.length; i++) {
+            containMap.put(nums2[i], i);
+        }
+        int[] answer = new int[nums1.length];
+        for (int i = 0; i < answer.length; i++) {
+            if (containMap.containsKey(nums1[i])) {
+                answer[i] = pre[containMap.get(nums1[i])];
+            }
+        }
+        return answer;
+    }
+
+}
