ab34cde873f9c02fbb2bda6548bb2f8565c51cc4
Andrey Litvitski <andrey1010102008@gmail.com>
2025-07-24 14:57:20 +0300

    new solution

---
+++ solutions/src/arrayPartition/ArrayPartition.java
@@ -0,0 +1,14 @@
+package arrayPartition;
+
+import java.util.Arrays;
+
+public class ArrayPartition {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        for (int i = nums.length - 1; i >= 0; i -= 2) {
+            sum += Math.min(nums[i], nums[i - 1]);
+        }
+        return sum;
+    }
+}
