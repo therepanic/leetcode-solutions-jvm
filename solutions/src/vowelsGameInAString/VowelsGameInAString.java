c234590b304247aebb306c2435c2ddeacc95113c
Andrey Litvitski <andrey1010102008@gmail.com>
2025-08-12 19:58:43 +0300

    new solution

---
+++ solutions/src/vowelsGameInAString/VowelsGameInAString.java
@@ -0,0 +1,12 @@
+package vowelsGameInAString;
+
+public class VowelsGameInAString {
+    public boolean doesAliceWin(String s) {
+        for (char c : s.toCharArray()) {
+            if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u') {
+                return true;
+            }
+        }
+        return false;
+    }
+}
