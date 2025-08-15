d315b94a73ad105f62eebbbd7a45d8cf9a8367d1
Andrey Litvitski <andrey1010102008@gmail.com>
2025-08-15 16:50:11 +0300

    new solution

---
+++ solutions/src/powerOfFour/PowerOfFour.java
@@ -0,0 +1,11 @@
+package powerOfFour;
+
+public class PowerOfFour {
+    public boolean isPowerOfFour(int n) {
+        if (n <= 0) return false;
+        while (n % 4 == 0) {
+            n /= 4;
+        }
+        return n == 1;
+    }
+}
