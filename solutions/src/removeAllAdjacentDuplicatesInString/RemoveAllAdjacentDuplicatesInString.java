5a05e0a2fc0d454699556298795b487988b1d479
Andrey Litvitski <andrey1010102008@gmail.com>
2025-09-11 22:51:53 +0300

    new solution

---
+++ solutions/src/removeAllAdjacentDuplicatesInString/RemoveAllAdjacentDuplicatesInString.java
@@ -0,0 +1,16 @@
+package removeAllAdjacentDuplicatesInString;
+
+public class RemoveAllAdjacentDuplicatesInString {
+    public String removeDuplicates(String s) {
+        StringBuilder sb = new StringBuilder(s);
+        for (int i = 0; i < sb.length() - 1; i++) {
+            if (i == -1) continue;
+            if (sb.charAt(i) == sb.charAt(i + 1)) {
+                sb.deleteCharAt(i);
+                sb.deleteCharAt(i);
+                i -= 2;
+            }
+        }
+        return sb.toString();
+    }
+}
