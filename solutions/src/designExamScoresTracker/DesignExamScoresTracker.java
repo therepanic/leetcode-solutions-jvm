d383acb6d3249b6dbcda41c08c4e2fe9dea463ac
Andrey Litvitski <andrey1010102008@gmail.com>
2025-10-11 18:06:16 +0300

    new solution

---
+++ solutions/src/designExamScoresTracker/DesignExamScoresTracker.java
@@ -0,0 +1,32 @@
+package designExamScoresTracker;
+
+import java.util.TreeMap;
+
+class ExamTracker {
+
+    private final TreeMap<Integer, Long> map = new TreeMap<>();
+    private final TreeMap<Integer, Long> prefix = new TreeMap<>();
+
+    public ExamTracker() {
+
+    }
+
+    public void record(int time, int score) {
+        map.put(time, (long) score);
+        long b = prefix.lowerEntry(time) != null ? prefix.lowerEntry(time).getValue() : 0;
+        long current = b;
+        for (var e : map.tailMap(time, true).entrySet()) {
+            current += e.getValue();
+            prefix.put(e.getKey(), current);
+        }
+    }
+
+    public long totalScore(int startTime, int endTime) {
+        var endEntry = prefix.floorEntry(endTime);
+        var startEntry = prefix.lowerEntry(startTime);
+        long endSum = endEntry != null ? endEntry.getValue() : 0;
+        long startSum = startEntry != null ? startEntry.getValue() : 0;
+        return endSum - startSum;
+    }
+}
+
