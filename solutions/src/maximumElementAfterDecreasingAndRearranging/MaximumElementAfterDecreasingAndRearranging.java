89a2fa0a315e31c8c499f8cae72ffe8b6b124214
Andrey Litvitski <andrey1010102008@gmail.com>
2025-11-27 18:13:13 +0300

    new solution

---
+++ solutions/src/maximumElementAfterDecreasingAndRearranging/MaximumElementAfterDecreasingAndRearranging.java
@@ -0,0 +1,18 @@
+package maximumElementAfterDecreasingAndRearranging;
+
+import java.util.Arrays;
+
+public class MaximumElementAfterDecreasingAndRearranging {
+    public int maximumElementAfterDecrementingAndRearranging(int[] arr) {
+        Arrays.sort(arr);
+        if (arr[0] != 1) {
+            arr[0] = 1;
+        }
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] - arr[i - 1] > 1) {
+                arr[i] = arr[i - 1] + 1;
+            }
+        }
+        return arr[arr.length - 1];
+    }
+}
