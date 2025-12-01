926a324b1d887c5475f3692c91ec40d21fa61625
Andrey Litvitski <andrey1010102008@gmail.com>
2025-12-01 17:55:54 +0300

    new solution

---
+++ solutions/src/checkIfAStringCanBreakAnotherString/CheckIfAStringCanBreakAnotherString.java
@@ -0,0 +1,30 @@
+package checkIfAStringCanBreakAnotherString;
+
+public class CheckIfAStringCanBreakAnotherString {
+    public boolean checkIfCanBreak(String s1, String s2) {
+        int[] first = new int[26];
+        int[] second = new int[26];
+        for (int i = 0; i < s1.length(); i++) {
+            first[s1.charAt(i) - 'a']++;
+            second[s2.charAt(i) - 'a']++;
+        }
+        boolean firstOk = true;
+        boolean secondOk = true;
+        int point = 0;
+        for (int i = 0; i < 26; i++) {
+            point += first[i] - second[i];
+            if (point < 0) {
+                firstOk = false;
+                break;
+            }
+        }
+        for (int i = 0; i < 26; i++) {
+            point += second[i] - first[i];
+            if (point < 0) {
+                secondOk = false;
+                break;
+            }
+        }
+        return firstOk || secondOk;
+    }
+}
