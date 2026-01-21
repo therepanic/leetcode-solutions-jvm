59b1a6b86d0e1c7dcc1e6e705ff104e7eb481a22
Andrey Litvitski <andrey1010102008@gmail.com>
2026-01-21 17:06:43 +0300

    new solution

---
+++ solutions/src/constructTheMinimumBitwiseArrayII/ConstructTheMinimumBitwiseArrayII.java
@@ -0,0 +1,20 @@
+package constructTheMinimumBitwiseArrayII;
+
+import java.util.List;
+
+public class ConstructTheMinimumBitwiseArrayII {
+    public int[] minBitwiseArray(List<Integer> nums) {
+        int[] ans = new int[nums.size()];
+        for (int i = 0; i < nums.size(); i++) {
+            int x = nums.get(i);
+            int r = -1;
+            int d = 1;
+            while ((x & d) != 0) {
+                r = x - d;
+                d <<= 1;
+            }
+            ans[i] = r;
+        }
+        return ans;
+    }
+}
