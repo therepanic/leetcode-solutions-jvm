e5cc696e2653f0ef3920b68efce7896f467b1df9
Andrey Litvitski <andrey1010102008@gmail.com>
2026-01-30 19:45:48 +0300

    new solution

---
+++ solutions/src/longestUncommonSubsequenceI/LongestUncommonSubsequenceI.java
@@ -0,0 +1,11 @@
+package longestUncommonSubsequenceI;
+
+public class LongestUncommonSubsequenceI {
+    public int findLUSlength(String a, String b) {
+        if (a.equals(b)) {
+            return -1;
+        } else {
+            return Math.max(a.length(), b.length());
+        }
+    }
+}
