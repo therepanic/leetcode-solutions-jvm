847035983a8c5cea542290db8150f3b865a2f68a
Andrey Litvitski <andrey1010102008@gmail.com>
2026-02-16 17:13:31 +0300

    new solution

---
+++ solutions/src/reverseBits/ReverseBits.java
@@ -0,0 +1,13 @@
+package reverseBits;
+
+public class ReverseBits {
+    public int reverseBits(int n) {
+        int res = 0;
+        for (int i = 0; i < 32; i++) {
+            res <<= 1;
+            res |= (n & 1);
+            n >>= 1;
+        }
+        return res;
+    }
+}
