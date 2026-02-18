f1d37fd66e4f40b09d817c709a476e683413ca56
Andrey Litvitski <andrey1010102008@gmail.com>
2026-02-18 16:12:25 +0300

    new solution

---
+++ solutions/src/binaryNumberWithAlternatingBits/BinaryNumberWithAlternatingBits.java
@@ -0,0 +1,15 @@
+package binaryNumberWithAlternatingBits;
+
+public class BinaryNumberWithAlternatingBits {
+    public boolean hasAlternatingBits(int n) {
+        int prev = n & 1;
+        n >>>= 1;
+        while (n != 0) {
+            int cur = n & 1;
+            if (cur == prev) return false;
+            prev = cur;
+            n >>>= 1;
+        }
+        return true;
+    }
+}
