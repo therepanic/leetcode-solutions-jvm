fa6e9dd63110e3fcc90689cbc1694d4a1ac2b369
Andrey Litvitski <andrey1010102008@gmail.com>
2026-02-20 22:29:29 +0300

    new solution

---
+++ solutions/src/kidsWithTheGreatestNumberOfCandies/KidsWithTheGreatestNumberOfCandies.java
@@ -0,0 +1,18 @@
+package kidsWithTheGreatestNumberOfCandies;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class KidsWithTheGreatestNumberOfCandies {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<>();
+        int max = -1;
+        for (int i = 0; i < candies.length; i++) {
+            max = Math.max(max, candies[i]);
+        }
+        for (int i = 0; i < candies.length; i++) {
+            ans.add(candies[i] + extraCandies >= max);
+        }
+        return ans;
+    }
+}
