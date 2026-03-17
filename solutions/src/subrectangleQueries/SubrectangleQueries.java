37e9dd737b61cbe3e364f5fe52ffb52652450b91
Andrey Litvitski <andrey1010102008@gmail.com>
2026-03-17 19:41:53 +0300

    new solution

---
+++ solutions/src/subrectangleQueries/SubrectangleQueries.java
@@ -0,0 +1,21 @@
+package subrectangleQueries;
+
+public class SubrectangleQueries {
+    private final int[][] rectangle;
+
+    public SubrectangleQueries(int[][] rectangle) {
+        this.rectangle = rectangle;
+    }
+
+    public void updateSubrectangle(int row1, int col1, int row2, int col2, int newValue) {
+        for (int i = row1; i <= row2; i++) {
+            for (int j = col1; j <= col2; j++) {
+                this.rectangle[i][j] = newValue;
+            }
+        }
+    }
+
+    public int getValue(int row, int col) {
+        return this.rectangle[row][col];
+    }
+}
