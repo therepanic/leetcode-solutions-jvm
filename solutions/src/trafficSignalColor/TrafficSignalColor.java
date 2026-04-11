e9419200005386e75fbcdc0cad37fe624ed70e46
Andrey Litvitski <andrey1010102008@gmail.com>
2026-04-11 18:48:42 +0300

    new solution

---
+++ solutions/src/trafficSignalColor/TrafficSignalColor.java
@@ -0,0 +1,10 @@
+package trafficSignalColor;
+
+public class TrafficSignalColor {
+    public String trafficSignal(int timer) {
+        if (timer == 0) return "Green";
+        if (timer == 30) return "Orange";
+        if (timer > 30 && timer <= 90) return "Red";
+        return "Invalid";
+    }
+}
