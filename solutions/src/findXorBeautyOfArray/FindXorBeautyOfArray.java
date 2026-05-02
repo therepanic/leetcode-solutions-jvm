6881f7c3ecde60844c1bab982a30419a18b1233e
Andrey Litvitski <andrey1010102008@gmail.com>
2026-05-02 14:15:56 +0300

    new solution

---
+++ solutions/src/findXorBeautyOfArray/FindXorBeautyOfArray.java
@@ -0,0 +1,11 @@
+package findXorBeautyOfArray;
+
+public class FindXorBeautyOfArray {
+    public int xorBeauty(int[] nums) {
+        int a = 0;
+        for (int i = 0; i < nums.length; i++) {
+            a ^= nums[i];
+        }
+        return a;
+    }
+}
