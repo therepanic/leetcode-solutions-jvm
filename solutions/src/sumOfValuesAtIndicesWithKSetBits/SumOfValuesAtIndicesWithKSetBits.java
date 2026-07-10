3c36ffa9b76ff02eb1ba28b639b3ef5c7c8602e5
Andrey Litvitski <andrey1010102008@gmail.com>
2026-07-10 17:16:17 +0300

    new solution

---
+++ solutions/src/sumOfValuesAtIndicesWithKSetBits/SumOfValuesAtIndicesWithKSetBits.java
@@ -0,0 +1,15 @@
+package sumOfValuesAtIndicesWithKSetBits;
+
+import java.util.List;
+
+public class SumOfValuesAtIndicesWithKSetBits {
+    public int sumIndicesWithKSetBits(List<Integer> nums, int k) {
+        int ans = 0;
+        for (int i = 0; i < nums.size(); i++) {
+            if (Integer.bitCount(i) == k) {
+                ans += nums.get(i);
+            }
+        }
+        return ans;
+    }
+}
