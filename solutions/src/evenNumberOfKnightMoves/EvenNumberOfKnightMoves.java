84e60c19b7db2f76a50ad7b3dd88d5c423139ae3
Andrey Litvitski <andrey1010102008@gmail.com>
2026-07-19 16:38:29 +0300

    new solution

---
+++ solutions/src/evenNumberOfKnightMoves/EvenNumberOfKnightMoves.java
@@ -0,0 +1,7 @@
+package evenNumberOfKnightMoves;
+
+public class EvenNumberOfKnightMoves {
+    public boolean canReach(int[] start, int[] target) {
+        return ((start[0] + start[1]) % 2) == ((target[0] + target[1]) % 2);
+    }
+}
