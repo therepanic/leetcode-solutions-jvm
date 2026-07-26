5d0dc8a05c6885cca09015951fa33b46045752b6
Andrey Litvitski <andrey1010102008@gmail.com>
2026-07-26 16:11:31 +0300

    new solution

---
+++ solutions/src/maximumProductOfThreeNumbers/MaximumProductOfThreeNumbers.java
@@ -0,0 +1,10 @@
+package maximumProductOfThreeNumbers;
+
+import java.util.Arrays;
+
+public class MaximumProductOfThreeNumbers {
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        return Math.max(nums[0] * nums[nums.length - 1] * nums[1], nums[nums.length - 1] * nums[nums.length - 2] * nums[nums.length - 3]);
+    }
+}
