aca0cf7d2bd64cff67ea406b4540c8f68cb4cda6
Andrey Litvitski <andrey1010102008@gmail.com>
2026-07-28 19:20:36 +0300

    new solution

---
+++ solutions/src/runningSumOf1dArray/RunningSumOf1dArray.java
@@ -0,0 +1,10 @@
+package runningSumOf1dArray;
+
+public class RunningSumOf1dArray {
+    public int[] runningSum(int[] nums) {
+        for (int i = 1; i < nums.length; i++) {
+            nums[i] += nums[i - 1];
+        }
+        return nums;
+    }
+}
