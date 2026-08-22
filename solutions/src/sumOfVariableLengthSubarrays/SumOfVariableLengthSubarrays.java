cece3008a80c10e4e72c59601eb23c3ba4d6aa75
Andrey Litvitski <andrey1010102008@gmail.com>
2026-08-22 19:02:52 +0300

    new solution

---
+++ solutions/src/sumOfVariableLengthSubarrays/SumOfVariableLengthSubarrays.java
@@ -0,0 +1,14 @@
+package sumOfVariableLengthSubarrays;
+
+public class SumOfVariableLengthSubarrays {
+    public int subarraySum(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int start = Math.max(0, i - nums[i]);
+            for (int j = start; j <= i; j++) {
+                sum += nums[j];
+            }
+        }
+        return sum;
+    }
+}
