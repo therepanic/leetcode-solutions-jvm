d6b81d069ed59b00ef90db2637c689c4aeeeb556
Andrey Litvitski <andrey1010102008@gmail.com>
2026-08-26 18:34:30 +0300

    new solution

---
+++ solutions/src/sumOfElementsWithFrequencyDivisibleByK/SumOfElementsWithFrequencyDivisibleByK.java
@@ -0,0 +1,21 @@
+package sumOfElementsWithFrequencyDivisibleByK;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class SumOfElementsWithFrequencyDivisibleByK {
+    public int sumDivisibleByK(int[] nums, int k) {
+        Map<Integer, Integer> sumMap = new HashMap<>();
+        Map<Integer, Integer> freqMap = new HashMap<>();
+        for (int v : nums) {
+            sumMap.put(v, sumMap.getOrDefault(v, 0) + 1);
+        }
+        int sum = 0;
+        for (var entry : sumMap.entrySet()) {
+            if (entry.getValue() % k == 0) {
+                sum += entry.getKey() * entry.getValue();
+            }
+        }
+        return sum;
+    }
+}
