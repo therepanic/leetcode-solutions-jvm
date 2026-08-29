864ab91f8c1414159e97ec6541613b3c659790db
Andrey Litvitski <andrey1010102008@gmail.com>
2026-08-29 17:34:30 +0300

    new solution

---
+++ solutions/src/concatenateArrayWithReverse/ConcatenateArrayWithReverse.java
@@ -0,0 +1,14 @@
+package concatenateArrayWithReverse;
+
+public class ConcatenateArrayWithReverse {
+    public int[] concatWithReverse(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+        }
+        for (int i = 0; i < nums.length; i++) {
+            ans[i + nums.length] = nums[nums.length - i - 1];
+        }
+        return ans;
+    }
+}
