0ef9297d243c0ee94a48b6ea4875fac1e99759aa
Andrey Litvitski <andrey1010102008@gmail.com>
2026-09-20 23:57:54 +0300

    new solutions

---
+++ solutions/src/numberOfIntersectingIntervalPairsII/NumberOfIntersectingIntervalPairsII.java
@@ -0,0 +1,28 @@
+package numberOfIntersectingIntervalPairsII;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class NumberOfIntersectingIntervalPairsII {
+    public long countIntersectingIntervals(int[][] intervals) {
+        long c = 0;
+        List<Integer> start = new ArrayList<>();
+        List<Integer> end = new ArrayList<>();
+        for (int i = 0; i < intervals.length; i++) {
+            start.add(intervals[i][0]);
+            end.add(intervals[i][1]);
+        }
+        Collections.sort(start);
+        Collections.sort(end);
+        int j = 0;
+        long ans = 0;
+        for (int i = 0; i < start.size(); i++) {
+            while (j < end.size() && start.get(i) > end.get(j)) {
+                j++;
+            }
+            ans += i - j;
+        }
+        return ans;
+    }
+}
